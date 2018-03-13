49c1a43cef8e2d356b2cd348924eec5322a34af7
PanYibin <>
2018-03-13 08:37:26 +0800

    add example code

--- Example/YBUtils/YBViewController.m
+++ Example/YBUtils/YBViewController.m
@@ -20,6 +20,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+    
+    [dict setObject:@"12" forKey:@"num"];
+    [dict setObject:@"abc" forKey:@"num1"];
+    [dict setObject:@[@"a", @"b", @"c"] forKey:@"num2"];
+    [dict setObject:@{@"name":@"hello"} forKey:@"num3"];
+    [dict setObject:@(YES) forKey:@"num4"];
+    
+    NSInteger a = [dict getIntegerForKey:@"num"];
+    NSString *b = [dict getStringForKey:@"num1"];
+    NSArray *array = [dict getArrayForKey:@"num2"];
+    NSDictionary *d = [dict getDictionaryForKey:@"num3"];
+    BOOL bo = [dict getBoolForKey:@"num4"];
+    
+    NSLog(@"finish");
 }
 
 - (void)didReceiveMemoryWarning
